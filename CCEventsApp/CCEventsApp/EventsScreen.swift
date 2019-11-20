72d6baedca64ca43571dcb785d491437ff835ee9
soohiaquick <sophia.quick17@gmail.com>
2019-11-20 13:02:42 -0700

    Changed the launch screen

---
+++ CCEventsApp/CCEventsApp/EventsScreen.swift
@@ -0,0 +1,30 @@
+//
+//  EventsScreen.swift
+//  CCEventsApp
+//
+//  Created by Sophia Quick on 11/20/19.
+//  Copyright © 2019 Sophia Quick. All rights reserved.
+//
+
+import UIKit
+
+class EventsScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
