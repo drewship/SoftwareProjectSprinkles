c9dd1e71f37493c2b6509ab2e3296004b6ed6134
soohiaquick <sophia.quick17@gmail.com>
2019-11-20 17:13:33 -0700

    Some new features

--- CCEventsApp/CCEventsApp/ViewController.swift
+++ CCEventsApp/CCEventsApp/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var whatTextField: UITextField!
+    @IBOutlet weak var timeTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var titleTextView: UITextView!
+    @IBOutlet weak var timeTextView: UITextView!
+    @IBOutlet weak var locationTextView: UITextView!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    @IBAction func submitButton(_ sender: UIButton) {
+          let titleText = whatTextField.text
+          let timeText = timeTextField.text
+          let locationText = locationTextField.text
+          let descriptionText = descriptionTextField.text
+          
+          titleTextView.text = titleText
+          timeTextView.text = timeText
+          locationTextView.text = locationText
+          descriptionTextView.text = descriptionText
+          
+      }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+  
+    
 }
 
